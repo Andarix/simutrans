90d9f43b9d1615193ed08d3bdd7bea49ba55e4fb
privat <privat@local.de>
2020-08-11 11:18:12 +0200

    CHG wget to curl

--- build_windows.cmd
+++ build_windows.cmd
@@ -6,3 +6,9 @@ del simutrans\Simutrans_GDI.pdb
 del simutrans\Simutrans_GDI.exe.manifest
 del simutrans\Simutrans_SDL2.pdb
 del simutrans\Simutrans_SDL2.exe.manifest
+
+libs\curl\curl.exe -L -d "version=0&choice=all&submit=Export%21" https://translator.simutrans.com/script/main.php?page=wrap >NULL
+libs\curl\curl.exe -L https://translator.simutrans.com/data/tab/language_pack-Base+texts.zip > language_pack-Base+texts.zip
+Expand-Archive -LiteralPath "language_pack-Base+texts.zip" -DestinationPath simutrans/text
+
+pause
