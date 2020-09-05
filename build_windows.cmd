34257036d520880b9722e9c2591f3913ab4d8c3a
privat <privat@local.de>
2020-09-05 19:20:20 +0200

    r9213

--- build_windows.cmd
+++ build_windows.cmd
@@ -2,15 +2,17 @@ git svn log --oneline --limit=1 > status.txt
 
 set /p string=<status.txt
 
-echo #define REVISION = %string:~1,5% > revision.h
+echo #define REVISION %string:~1,5% > revision.h
 
 libs\buildTool\MSBuild\Current\Bin\MSBuild.exe makeobj\Makeobj.vcxproj -property:Configuration=Release
 libs\buildTool\MSBuild\Current\Bin\MSBuild.exe Simutrans-GDI.vcxproj -property:Configuration=Debug
 libs\buildTool\MSBuild\Current\Bin\MSBuild.exe Simutrans-SDL2.vcxproj -property:Configuration=Debug
+libs\buildTool\MSBuild\Current\Bin\MSBuild.exe Simutrans-Server.vcxproj -property:Configuration=Debug
 
-copy Simutrans_GDI.exe simutrans/Simutrans_GDI.exe
-copy Simutrans_SDL2.exe simutrans/Simutrans_SDL2.exe
-copy libs/SDL2.dll simutrans/SDL2.dll
+copy Simutrans_GDI.exe simutrans\Simutrans_GDI.exe
+copy Simutrans_SDL2.exe simutrans\Simutrans_SDL2.exe
+copy Simutrans_Server.exe simutrans\Simutrans_SDL2.exe
+copy libs/SDL2.dll simutrans\SDL2.dll
 
 
 rem libs\curl\curl.exe https://translator.simutrans.com/script/main.php?page=wrap
--- revision.h
+++ revision.h
@@ -1,1 +1,1 @@
-#define REVISION 9210  
+#define REVISION 9213  
