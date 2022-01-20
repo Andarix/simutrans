Andarix/simutrans

commit 7afb9e976d6ef0a0b47206907c0a52b4cf2f6a7d

FIX: delete double entries in schedules while editing, fix memory leak
git-svn-id: svn://5.135.125.166/simutrans/trunk@10372 8aca7d54-2c30-db11-9de9-000461428c89

gui/components/gui_schedule.cc
@@ -442,8 +442,6 @@ void gui_schedule_t::init(schedule_t* schedule_, player_t* player, convoihandle_
 		if( old_schedule ) {
 			stats->highlight_schedule( false );
 			update_tool( false );
-			schedule->cleanup();
-			schedule->finish_editing();
 		}
 
 		// initialization
@@ -453,6 +451,7 @@ void gui_schedule_t::init(schedule_t* schedule_, player_t* player, convoihandle_
 		current_schedule_rotation = welt->get_settings().get_rotation();
 
 		// prepare editing
+		delete schedule;
 		schedule = schedule_->copy();
 
 		make_return = (schedule->get_waytype() == road_wt || schedule->get_waytype() == air_wt || schedule->get_waytype() == water_wt);
@@ -650,6 +649,9 @@ void gui_schedule_t::draw(scr_coord pos)
 			current_schedule_rotation = (current_schedule_rotation + 1) % 4;
 			schedule->rotate90( (4+current_schedule_rotation - world_rotation) & 1 ? world()->get_size().x-1 : world()->get_size().y-1 );
 		}
+
+		// remove double entries
+		schedule->cleanup();
 
 		schedule_t *scd = get_old_schedule();
 		// change current entry while convois drives on

commit 4974488ed3c321ba23fe3a942af20a4257747960

FIX crash introduced in r10372
git-svn-id: svn://5.135.125.166/simutrans/trunk@10379 8aca7d54-2c30-db11-9de9-000461428c89

gui/components/gui_schedule.cc
@@ -451,7 +451,9 @@ void gui_schedule_t::init(schedule_t* schedule_, player_t* player, convoihandle_
 		current_schedule_rotation = welt->get_settings().get_rotation();
 
 		// prepare editing
-		delete schedule;
+		if (schedule != schedule_) {
+			delete schedule;
+		}
 		schedule = schedule_->copy();
 
 		make_return = (schedule->get_waytype() == road_wt || schedule->get_waytype() == air_wt || schedule->get_waytype() == water_wt);