31d366cc9039edfc14f950fc1976da61f6bf4f5c
Andarix <fx78p@arcor.de>
2022-04-08 16:30:27 +0200

    CHG android folder

--- src/android/debug.bat
+++ src/android/debug.bat
@@ -7,6 +7,6 @@ set "tablet=emulator-5554"
 cd C:\temp\platform-tools
 adb -s %tablet% shell stop
 adb -s %tablet% shell setprop log.redirect-stdio true
-adb -s %tablet% shell am start com.simutrans-germany/.MainActivity --es args "'-log -debug 3"'
+adb -s %tablet% shell am start com.simutrans.germany.simutrans/.MainActivity --es args "'-log -debug 3"'
 adb -s %tablet% shell start
-adb -s %tablet% logcat com.simutrans:V *:S
+adb -s %tablet% logcat com.simutrans.germany.simutrans:V *:S
