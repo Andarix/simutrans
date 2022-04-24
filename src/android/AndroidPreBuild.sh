b3e44a8fa27f3e7d6320481c6d83d9f7c5dce51c
Andarix <fx78p@arcor.de>
2022-04-24 20:59:59 +0200

    Android build

--- src/android/AndroidPreBuild.sh
+++ src/android/AndroidPreBuild.sh
@@ -34,9 +34,10 @@ cd simutrans
 [ -e simutrans/pak ] || (download_with_retry http://downloads.sourceforge.net/project/simutrans/pak64/123-0/simupak64-123-0.zip simupak64-123-0.zip && unzip -n simupak64-123-0.zip -d .) || exit 1
 [ -e ../../simutrans/pak.japan ] || (download_with_retry http://downloads.sourceforge.net/project/simutrans/pak64.japan/123-0/simupak64.japan-123-0.zip simupak64.japan-123-0.zip && unzip -n simupak64.japan-123-0.zip -d .) || exit 1
 [ -e ../simutrans/pak64.german ] || (download_with_retry https://simutrans-germany.com/pak.german/pak64.german_0-123-0-0-5_full.zip pak64.german_0-123-0-0-5_full.zip && unzip -n pak64.german_0-123-0-0-5_full.zip -d .) || exit 1
-mkdir simutrans/music
-[ -e simutrans/music/TimGM6mb.sf2 ] || (download_with_retry https://sourceforge.net/p/mscore/code/HEAD/tree/trunk/mscore/share/sound/TimGM6mb.sf2?format=raw TimGM6mb.sf2 && cp ./TimGM6mb.sf2 simutrans/music/TimGM6mb.sf2) || exit 1
+cd music
+[ -e simutrans/music/TimGM6mb.sf2 ] || (download_with_retry https://sourceforge.net/p/mscore/code/HEAD/tree/trunk/mscore/share/sound/TimGM6mb.sf2?format=raw TimGM6mb.sf2) || exit 1
 #[ -e ../simutrans/font/RobotoCondensed-Regular.ttf ] || (download_with_retry https://fonts.google.com/download?family=Roboto%20Condensed Roboto_Condensed.zip && unzip -n Roboto_Condensed.zip -d ../simutrans/font) || exit 1
+cd ..
 [ -e simutrans/font/Roboto-Regular.ttf ] || (download_with_retry https://fonts.google.com/download?family=Roboto Roboto.zip && unzip -n Roboto.zip Roboto-Regular.ttf -d simutrans/font) || exit 1
 [ -e simutrans/cacert.pem ] || cp src/android/cacert.pem simutrans/cacert.pem || exit 1
 [ -e simutrans/get_pak.sh ] || cp src/android/unpak.sh simutrans/get_pak.sh; chmod 755 simutrans/get_pak.sh  || exit 1
@@ -53,4 +54,4 @@ zip -r -0 ../../AndroidData/data.zip * >/dev/null
 cd ../..
 echo Generating data.zip done
 
-[ -e AndroidData/TimGM6mb.sf2 ] || cp simutrans/TimGM6mb.sf2 AndroidData/ || exit 1
+#[ -e AndroidData/TimGM6mb.sf2 ] || cp simutrans/TimGM6mb.sf2 AndroidData/ || exit 1
