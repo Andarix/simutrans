c2cf9a982b50e9cef81c6a3d45839e874d2c9057
Andarix <fx78p@arcor.de>
2023-03-06 08:48:11 +0100

    r10878

--- _svn-merge_revision.cmd
+++ _svn-merge_revision.cmd
@@ -16,11 +16,11 @@ git commit -am "r%string:~1,5%"
 
 pause
 
-git push --delete "simutrans_build" origin Nightly
+git push --delete "simutrans_build" master Nightly
 git tag -d Nightly
 
 git tag -a Nightly -m ""
 
-git push --tags --progress "simutrans_build" origin
+git push --tags --progress "simutrans_build" master
 
 pause
