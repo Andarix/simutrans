Andarix/simutrans

commit 4e89560c19f07bcaeab294ee191362367fbb88e5

FIX: correct default paremeter for station scripting
git-svn-id: svn://servers.simutrans.org/simutrans/trunk@11484 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/tool/simtool.cc
@@ -4594,9 +4594,9 @@ DBG_MESSAGE("tool_station_aux()", "building %s on square %d,%d for waytype %x", 
 			}
 
 			switch(ribi) {
-				//case ribi_t::south:layout = 0;  break;
-				case ribi_t::east:   layout = 1;    break;
-				case ribi_t::north:  layout = 2;    break;
+				case ribi_t::south: layout = 0;  break;
+				case ribi_t::east:  layout = 1;    break;
+				case ribi_t::north: layout = 2;    break;
 				case ribi_t::west:  layout = 3;    break;
 			}
 		}
@@ -4650,6 +4650,7 @@ DBG_MESSAGE("tool_station_aux()", "building %s on square %d,%d for waytype %x", 
 			// now for the details
 			ribi_t::ribi senkrecht = ~ribi_t::doubles(ribi);
 			ribi_t::ribi waagerecht = ribi_t::doubles(ribi);
+			layout = 0;
 			if(next_own!=ribi_t::none) {
 				// oriented buildings here
 				if(ribi_t::is_single(ribi & next_own)) {
@@ -4678,6 +4679,7 @@ DBG_MESSAGE("tool_station_aux()", "building %s on square %d,%d for waytype %x", 
 					}
 				}
 			}
+			assert(layout >= 0);
 			// avoid orientation on 8 tiled buildings
 			layout &= (desc->get_all_layouts()-1);
 		}

src/simutrans/script/api/api_command.cc
@@ -329,7 +329,12 @@ call_tool_work build_station_rotation(player_t* pl, koord3d pos, const building_
 	}
 	static cbuffer_t buf;
 	buf.clear();
-	buf.printf("%s,%i", building->get_name(), rot);
+	if (rot >= 0) {
+		buf.printf("%s,%i", building->get_name(), rot);
+	}
+	else {
+		buf.printf("%s", building->get_name());
+	}
 	return call_tool_work(TOOL_BUILD_STATION | GENERAL_TOOL, buf, 0, pl, pos);
 }
 

commit c4c84501498de6e4305e40f4f5a2449815c79871

Fix stations being built in wrong layout
Bug was introduced in r11484.

git-svn-id: svn://servers.simutrans.org/simutrans/trunk@11510 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/tool/simtool.cc
@@ -4650,7 +4650,7 @@ DBG_MESSAGE("tool_station_aux()", "building %s on square %d,%d for waytype %x", 
 			// now for the details
 			ribi_t::ribi senkrecht = ~ribi_t::doubles(ribi);
 			ribi_t::ribi waagerecht = ribi_t::doubles(ribi);
-			layout = 0;
+
 			if(next_own!=ribi_t::none) {
 				// oriented buildings here
 				if(ribi_t::is_single(ribi & next_own)) {