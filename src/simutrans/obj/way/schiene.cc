Andarix/simutrans

commit 70f80b1acd66e9d23b657eb35f022127ec625b06

Fixed reservation and revervation display for close diagonals
git-svn-id: svn://servers.simutrans.org/simutrans/trunk@12041 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/obj/way/schiene.cc
@@ -17,6 +17,7 @@
 
 #include "../../descriptor/way_desc.h"
 #include "../../builder/wegbauer.h"
+#include "../../player/simplay.h"
 
 #include "schiene.h"
 
@@ -27,6 +28,7 @@ bool schiene_t::show_reservations = false;
 schiene_t::schiene_t() : weg_t()
 {
 	reserved2 = reserved = convoihandle_t();
+	enter = enter2 = 0;
 
 	if (schiene_t::default_schiene) {
 		set_desc(schiene_t::default_schiene);
@@ -40,6 +42,8 @@ schiene_t::schiene_t() : weg_t()
 schiene_t::schiene_t(loadsave_t *file) : weg_t()
 {
 	reserved2 = reserved = convoihandle_t();
+	enter = enter2 = 0;
+
 	rdwr(file);
 }
 
@@ -80,14 +84,24 @@ void schiene_t::info(cbuffer_t & buf) const
 /**
  * true, if this rail can be reserved
  */
-bool schiene_t::reserve(convoihandle_t c, ribi_t::ribi dir  )
+bool schiene_t::reserve(convoihandle_t c, uint32 index )
 {
 	if (is_close_diagonal()) {
-		convoihandle_t& r = (is_close_diagonal() == 2 && (dir & ribi_t::southeast)) || (is_close_diagonal() == 1 && (dir & ribi_t::northeast)) ? reserved : reserved2;
+		// get entry direction
+		ribi_t::ribi from = ribi_type(c->get_route()->at(index-1), get_pos());
+		bool use2 = (is_close_diagonal() == 2 && (from & ribi_t::northeast)) || (is_close_diagonal() == 1 && (from & ribi_t::southeast));
+		convoihandle_t& r = use2 ? reserved2 : reserved;
+		convoihandle_t& r_other = use2 ? reserved : reserved2;
+		if (r_other == c) {
+			// may happen during rotations that we are rereved on the other track ...
+			r_other = convoihandle_t();
+			(use2 ? enter : enter2) = 0;
+		}
 		if (r.is_bound() && r != c) {
 			return false;
 		}
 		r = c;
+		(use2 ? enter2 : enter) = from;
 		if (schiene_t::show_reservations) {
 			set_flag(obj_t::dirty);
 		}
@@ -99,12 +113,16 @@ bool schiene_t::reserve(convoihandle_t c, ribi_t::ribi dir  )
 		 * direction is a diagonal (i.e. on the switching part)
 		 * and there are switching graphics
 		 */
-		if(  ribi_t::is_threeway(get_ribi_unmasked())  &&  ribi_t::is_bend(dir)  &&  get_desc()->has_switch_image()  ) {
-			mark_image_dirty( get_image(), 0 );
-			mark_image_dirty( get_front_image(), 0 );
-			set_switched((dir == ribi_t::northeast || dir == ribi_t::southwest));
-			set_images(image_switch, get_ribi_unmasked(), is_snow(), get_switched() );
-			set_flag( obj_t::dirty );
+		if(  index > 1  &&  ribi_t::is_threeway(get_ribi_unmasked())  &&  get_desc()->has_switch_image()  ) {
+			route_t const& rt = *c->get_route();
+			ribi_t::ribi dir = ribi_type(rt[index - 1], get_pos());
+			if (ribi_t::is_bend(dir)) {
+				mark_image_dirty(get_image(), 0);
+				mark_image_dirty(get_front_image(), 0);
+				set_switched((dir == ribi_t::northeast || dir == ribi_t::southwest));
+				set_images(image_switch, get_ribi_unmasked(), is_snow(), get_switched());
+				set_flag(obj_t::dirty);
+			}
 		}
 		if(schiene_t::show_reservations) {
 			set_flag( obj_t::dirty );
@@ -138,15 +156,17 @@ bool schiene_t::unreserve(vehicle_t *v)
 bool schiene_t::unreserve(convoihandle_t c)
 {
 	// is this tile reserved by us?
-	if(reserved.is_bound()  &&  reserved==c) {
+	if(reserved==c) {
 		reserved = convoihandle_t();
+		enter = 0;
 		if(schiene_t::show_reservations) {
 			set_flag( obj_t::dirty );
 		}
 		return true;
 	}
-	if (reserved2.is_bound()  &&  reserved2 == c) {
+	if (reserved2 == c) {
 		reserved2 = convoihandle_t();
+		enter2 = 0;
 		if (schiene_t::show_reservations) {
 			set_flag(obj_t::dirty);
 		}
@@ -206,14 +226,68 @@ void schiene_t::rdwr(loadsave_t *file)
 }
 
 
+void schiene_t::rotate90()
+{
+	weg_t::rotate90();
+	if (is_close_diagonal()) {
+		// it is unfourtunately not straight forward to swap the reservations ...
+		bool swap = false;
+		// bool use2 = (is_close_diagonal() == 2 && (from & ribi_t::northeast)) || (is_close_diagonal() == 1 && (from & ribi_t::southeast));
+		if (enter) {
+			swap = (is_close_diagonal() == 2 && (enter & ribi_t::northeast)) || (is_close_diagonal() == 1 && (enter & ribi_t::southeast));
+		}
+		else if (enter2) {
+			swap = (is_close_diagonal() == 2 && (enter2 & ribi_t::northeast)) || (is_close_diagonal() == 1 && (enter2 & ribi_t::southeast));
+		}
+		if (swap) {
+			// needs to swap convoi handles
+			convoihandle_t tmp = reserved2;
+			reserved2 = reserved;
+			reserved = tmp;
+			ribi_t::ribi t = enter2;
+			enter2 = enter;
+			enter = t;
+		}
+	}
+}
+
+
 FLAGGED_PIXVAL schiene_t::get_outline_colour() const
 {
 	if (env_t::show_single_ways  &&  ribi_t::is_single(get_ribi_unmasked())) {
-		return TRANSPARENT75_FLAG | OUTLINE_FLAG | gfx->palette_lookup(COL_RED);
+		return TRANSPARENT75_FLAG | OUTLINE_FLAG | gfx->palette_lookup(COL_YELLOW);
 	}
 	if (show_reservations  &&  reserved.is_bound()) {
 		return TRANSPARENT75_FLAG | OUTLINE_FLAG | gfx->palette_lookup(COL_RED);
 	}
-
+	if (show_reservations  &&  is_close_diagonal()  &&  get_desc()->has_close_diagonal_image() && reserved2.is_bound()) {
+		return TRANSPARENT75_FLAG | OUTLINE_FLAG | gfx->palette_lookup(COL_RED);
+	}
 	return 0;
+}
+
+
+void schiene_t::display(int xpos, int ypos CLIP_NUM_DEF) const
+{
+	if (is_close_diagonal() && !get_desc()->has_close_diagonal_image()) {
+		image_id image = get_desc()->get_diagonal_image_id(is_close_diagonal() == 1 ? ribi_t::northwest : ribi_t::southwest, is_snow());
+		if (get_owner_nr() != PLAYER_UNOWNED) {
+			if (obj_t::show_owner) {
+				gfx->draw_blend(image, xpos, ypos, get_owner_nr(), gfx->palette_lookup(get_owner()->get_player_color1() + 2) | OUTLINE_FLAG | TRANSPARENT75_FLAG, 0, get_flag(dirty)  CLIP_NUM_PAR);
+			}
+			else {
+				gfx->draw_color(image, xpos, ypos, get_owner_nr(), true, get_flag(dirty)  CLIP_NUM_PAR);
+			}
+		}
+		else {
+			gfx->draw_normal(image, xpos, ypos, 0, true, get_flag(dirty)  CLIP_NUM_PAR);
+		}
+
+		if (show_reservations  &&  reserved2.is_bound()) {
+			// transparent outline for second way
+			gfx->draw_blend(image, xpos, ypos, get_owner_nr(), TRANSPARENT75_FLAG | OUTLINE_FLAG | gfx->palette_lookup(COL_RED), 0, get_flag(dirty)  CLIP_NUM_PAR);
+		}
+	}
+
+	obj_t::display(xpos, ypos CLIP_NUM_PAR);
 }

src/simutrans/obj/way/schiene.h
@@ -26,6 +26,8 @@ protected:
 	// only used for closed_diagonals
 	convoihandle_t reserved2;
 
+	ribi_t::ribi enter, enter2;
+
 public:
 	static const way_desc_t *default_schiene;
 
@@ -65,7 +67,7 @@ public:
 	/**
 	* true, then this rail was reserved
 	*/
-	bool reserve(convoihandle_t c, ribi_t::ribi dir);
+	bool reserve(convoihandle_t c, uint32 route_index);
 
 	/**
 	* releases previous reservation
@@ -89,6 +91,9 @@ public:
 
 	void rdwr(loadsave_t *file) OVERRIDE;
 
+	// may need to swap reservations
+	void rotate90() OVERRIDE;
+
 	/**
 	 * if a function return here a value with TRANSPARENT_FLAGS set
 	 * then a transparent outline with the color form the lower 8 Bit is drawn
@@ -99,6 +104,8 @@ public:
 	 * to show reservations if needed
 	 */
 	image_id get_outline_image() const OVERRIDE { return weg_t::get_image(); }
+
+	void display(int xpos, int ypos  CLIP_NUM_DEF) const OVERRIDE;
 };
 
 

src/simutrans/obj/way/weg.cc
@@ -255,6 +255,10 @@ void weg_t::rdwr(loadsave_t *file)
 	if(  file->is_loading()  ) {
 		ribi = dummy8 & 15; // before: high bits was maske
 		ribi_maske = 0; // maske will be restored by signal/roadsign
+		if (ribi == ribi_t::all) {
+			// will be recaluclated later but may be needed for loading convois
+			close_diagonal_state = 1;
+		}
 	}
 
 	uint16 dummy16=max_speed;
@@ -551,7 +555,7 @@ void weg_t::calc_image()
 					set_images(image_close_diagonal, is_close_diagonal(), snow);
 				}
 				else {
-					set_images(image_diagonal, is_close_diagonal() == 1 ? ribi_t::northwest : ribi_t::southwest, snow);
+					set_images(image_diagonal, is_close_diagonal() == 1 ? ribi_t::southeast : ribi_t::northeast, snow);
 				}
 			}
 		}
@@ -766,7 +770,7 @@ FLAGGED_PIXVAL weg_t::get_outline_colour() const
 void weg_t::display(int xpos, int ypos CLIP_NUM_DEF) const
 {
 	if (is_close_diagonal() && !desc->has_close_diagonal_image()) {
-		image_id image = desc->get_diagonal_image_id(is_close_diagonal() == 1 ? ribi_t::southeast : ribi_t::northeast, is_snow());
+		image_id image = get_desc()->get_diagonal_image_id(is_close_diagonal() == 1 ? ribi_t::northwest : ribi_t::southwest, is_snow());
 		if (get_owner_nr() != PLAYER_UNOWNED) {
 			if (obj_t::show_owner) {
 				gfx->draw_blend(image, xpos, ypos, get_owner_nr(), gfx->palette_lookup(get_owner()->get_player_color1() + 2) | OUTLINE_FLAG | TRANSPARENT75_FLAG, 0, get_flag(dirty)  CLIP_NUM_PAR);
@@ -777,6 +781,15 @@ void weg_t::display(int xpos, int ypos CLIP_NUM_DEF) const
 		}
 		else {
 			gfx->draw_normal(image, xpos, ypos, 0, true, get_flag(dirty)  CLIP_NUM_PAR);
+		}
+
+		const FLAGGED_PIXVAL transparent = get_outline_colour();
+		if (transparent) {
+			// transparency?
+			if (TRANSPARENT_FLAGS & transparent) {
+				// only transparent outline
+				gfx->draw_blend(image, xpos, ypos, get_owner_nr(), transparent, 0, get_flag(dirty)  CLIP_NUM_PAR);
+			}
 		}
 	}
 	obj_t::display(xpos, ypos CLIP_NUM_PAR);

src/simutrans/simconvoi.cc
@@ -285,7 +285,7 @@ void convoi_t::reserve_route()
 		for(  route_t::index_t idx = back()->get_route_index();  idx < next_reservation_index  /*&&  idx < route.get_count()*/;  idx++  ) {
 			if(  grund_t *gr = welt->lookup( route.at(idx) )  ) {
 				if(  schiene_t *sch = (schiene_t *)gr->get_weg( front()->get_waytype() )  ) {
-					sch->reserve( self, ribi_type( route.at(max(1u,idx)-1u), route.at(min(route.get_count()-1u,idx+1u)) ) );
+					sch->reserve( self, idx );
 				}
 			}
 		}
@@ -504,7 +504,7 @@ DBG_MESSAGE("convoi_t::finish_rd()","next_stop_index=%d", next_stop_index );
 				grund_t *gr=welt->lookup(v->get_pos());
 				// airplanes may have no ground ...
 				if (schiene_t* const sch0 = obj_cast<schiene_t>(gr->get_weg(fahr[i]->get_waytype()))) {
-					sch0->reserve(self,ribi_t::none);
+					sch0->reserve(self, fahr[i]->get_route_index());
 				}
 			}
 			fahr[0]->set_leading(true);
@@ -2183,7 +2183,8 @@ void convoi_t::vorfahren()
 			// eventually reserve this
 			vehicle_t const& v = *fahr[i];
 			if (schiene_t* const sch0 = obj_cast<schiene_t>(welt->lookup(v.get_pos())->get_weg(v.get_waytype()))) {
-				sch0->reserve(self,ribi_t::none);
+				// since reserve expect the entrance tile position
+				sch0->reserve(self,v.get_route_index()-1);
 			}
 			else {
 				break;
@@ -2397,11 +2398,14 @@ void convoi_t::rdwr(loadsave_t *file)
 					}
 					state = INITIAL;
 				}
-				// add to blockstrecke
+				// ,aybe reserve tile
 				if (v->get_waytype() != air_wt) {
-					// only for no airplanes, they are not allowed to cross
+					// airplanes are complicated and not done here
+
+					// full reservations are restored in set convoi later
 					if (schiene_t* sch = dynamic_cast<schiene_t*>(gr->get_weg(v->get_waytype()))) {
-						sch->reserve(self, ribi_t::none);
+						sch->calc_image();	// to get close diagonals right
+						sch->reserve(self, v->get_route_index()-1);
 					}
 
 					// add to crossing

src/simutrans/vehicle/air_vehicle.cc
@@ -476,7 +476,7 @@ bool air_vehicle_t::block_reserver( uint32 start, uint32 end, bool reserve ) con
 			if(reserve) {
 				start_now = true;
 				sch1->add_convoi_reservation(cnv->self);
-				bool can_reserve_this = sch1->reserve(cnv->self, ribi_t::none);
+				bool can_reserve_this = sch1->reserve(cnv->self, i);
 				if (can_reserve_this) {
 					// check of airplanes
 					for (uint8 i = 1; i < gr->obj_count(); i++) {

src/simutrans/vehicle/rail_vehicle.cc
@@ -778,14 +778,14 @@ bool rail_vehicle_t::block_reserver(const route_t *route, route_t::index_t start
 					next_signal_index = i;
 				}
 			}
-			if (!sch1->reserve(cnv->self, ribi_type(route->at(max(1u,i)-1u), route->at(min(route->get_count()-1u, i+1u))))) {
+			if (!sch1->reserve(cnv->self, i)) {
 				success = false;
 			}
 			if (gr->has_two_ways()) {
 				// we may need to reserve the other way as well
 				if (schiene_t* sch0 = dynamic_cast<schiene_t*>(gr->get_weg_nr(gr->get_weg_nr(0) == sch1))) {
 					// the other way is reservable too => try to reserve it
-					if (!sch0->reserve(cnv->self, ribi_type(route->at(max(1u,i)-1u), route->at(min(route->get_count()-1u, i+1u))))) {
+					if (!sch0->reserve(cnv->self, i)) {
 						success = false;
 					}
 				}
@@ -896,7 +896,7 @@ void rail_vehicle_t::enter_tile(grund_t* gr)
 		sch0->book(cargo, WAY_STAT_GOODS);
 		if(leading) {
 			sch0->book(1, WAY_STAT_CONVOIS);
-			sch0->reserve( cnv->self, get_direction() );
+			sch0->reserve( cnv->self, get_route_index()-1 );
 		}
 	}
 }

commit 1a8d73b419d1830e598fd98e386ac5fda77a37d5

FIX: (Nazalassa) restore switching graphics broken since r12041
r12041 changed the switch-image direction calculation from the route
direction through the turnout to the direction from the previous route
tile to the turnout itself. This always produces a single ribi, so the
bend condition never becomes true and switching graphics are never
selected.

Restore the through-turnout direction using the previous and next route
tiles and accept valid turnouts at route index 1.


git-svn-id: svn://servers.simutrans.org/simutrans/trunk@12209 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/obj/way/schiene.cc
@@ -113,9 +113,9 @@ bool schiene_t::reserve(convoihandle_t c, uint32 index )
 		 * direction is a diagonal (i.e. on the switching part)
 		 * and there are switching graphics
 		 */
-		if(  index > 1  &&  ribi_t::is_threeway(get_ribi_unmasked())  &&  get_desc()->has_switch_image()  ) {
+		if(  index >= 1  &&  index < c->get_route()->get_count() - 1  &&  ribi_t::is_threeway(get_ribi_unmasked())  &&  get_desc()->has_switch_image()  ) {
 			route_t const& rt = *c->get_route();
-			ribi_t::ribi dir = ribi_type(rt[index - 1], get_pos());
+			ribi_t::ribi dir = ribi_type(rt[index - 1], rt[index + 1]);
 			if (ribi_t::is_bend(dir)) {
 				mark_image_dirty(get_image(), 0);
 				mark_image_dirty(get_front_image(), 0);